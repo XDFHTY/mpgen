b50d5d2583e08d21e8c4220c50102bf2c5406b79
a24568744 <2393803535@qq.com>
2022-03-17 17:35:46 +0800

    更新mpcoder

--- src/main/java/com/cj/mpgen/util/gen/CodeGenerator.java
+++ src/main/java/com/cj/mpgen/util/gen/CodeGenerator.java
@@ -1,138 +1,138 @@
-package com.cj.mpgen.util;
-
-
-import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
-import com.baomidou.mybatisplus.generator.config.GlobalConfig;
-import com.baomidou.mybatisplus.generator.config.PackageConfig;
-import com.baomidou.mybatisplus.generator.config.StrategyConfig;
-import com.baomidou.mybatisplus.generator.config.rules.DateType;
-import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
-
-
-/**
- * @description: 代码生成器
- * @author: liuergu
- * @date: 2019/7/23
- * 参考官方文档：https://mp.baomidou.com/config/generator-config.html#%E5%9F%BA%E6%9C%AC%E9%85%8D%E7%BD%AE
- **/
-public class CodeGenerator {
-
-    public static void main(String[] args) {
-        // ================= 需要修改的配置 =================
-
-        // 数据源配置
-        String jdbcUrl = "jdbc:mysql://47.103.64.182:63306/ujie_v5_dev";
-//        String jdbcUrl = "jdbc:mysql://139.224.195.76:63306/emp";
-        String jdbcDriver = "com.mysql.jdbc.Driver";
-        String jdbcUsername = "root";
-        String jdbcPassword = "rootpass";
-
-        // 父级包名配置
-        String parentPackage = "com.cj.shard";
-
-        // 生成代码的 @author 值
-        String author = "zzz";
-
-        // 要生成代码的表名配置
-        String[] tables = {
-//                "v6_aio_data",
-//                "v6_aio_result",
-                "v6_aio_equ",
-//                "v5_hardware",
-//                "v5_hard_log",
-//                "emp_health_data_equipment_original",
-//                "emp_health_data_member_record",
-//                "emp_health_equipment",
-//                "emp_health_member",
-//                "emp_health_member_info",
-//                "emp_health_member_wx",
-//                "emp_health_relation_tenant_member",
-
-        };
-
+//package com.cj.mpgen.util;
+//
+//
+//import com.baomidou.mybatisplus.generator.AutoGenerator;
+//import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+//import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+//import com.baomidou.mybatisplus.generator.config.PackageConfig;
+//import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+//import com.baomidou.mybatisplus.generator.config.rules.DateType;
+//import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+//import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+//
+//
+///**
+// * @description: 代码生成器
+// * @author: liuergu
+// * @date: 2019/7/23
+// * 参考官方文档：https://mp.baomidou.com/config/generator-config.html#%E5%9F%BA%E6%9C%AC%E9%85%8D%E7%BD%AE
+// **/
+//public class CodeGenerator {
+//
+//    public static void main(String[] args) {
+//        // ================= 需要修改的配置 =================
+//
+//        // 数据源配置
+//        String jdbcUrl = "jdbc:mysql://47.103.64.182:63306/ujie_v5_dev";
+////        String jdbcUrl = "jdbc:mysql://139.224.195.76:63306/emp";
+//        String jdbcDriver = "com.mysql.jdbc.Driver";
+//        String jdbcUsername = "root";
+//        String jdbcPassword = "rootpass";
+//
+//        // 父级包名配置
+//        String parentPackage = "com.cj.shard";
+//
+//        // 生成代码的 @author 值
+//        String author = "zzz";
+//
+//        // 要生成代码的表名配置
 //        String[] tables = {
-//                "wx_ecg_msg",
+////                "v6_aio_data",
+////                "v6_aio_result",
+//                "v6_aio_equ",
+////                "v5_hardware",
+////                "v5_hard_log",
+////                "emp_health_data_equipment_original",
+////                "emp_health_data_member_record",
+////                "emp_health_equipment",
+////                "emp_health_member",
+////                "emp_health_member_info",
+////                "emp_health_member_wx",
+////                "emp_health_relation_tenant_member",
+//
 //        };
-
-        // ==================================================
-
-
-        // 全局配置
-        GlobalConfig gc = new GlobalConfig();
-        String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir(projectPath + "/src/main/java")
-                .setAuthor(author)
-                .setBaseResultMap(true)
-                .setBaseColumnList(true)
-                // 生成完毕后是否打开输出目录
-                .setOpen(false)
-                // 为true时生成entity将继承Model类，单类即可完成基于单表的业务逻辑操作，按需开启
-                .setActiveRecord(false)
-                //使用Date作为时间类型
-                .setDateType(DateType.ONLY_DATE)
-                .setSwagger2(true);
-
-
-        // 数据源配置
-        DataSourceConfig dsc = new DataSourceConfig();
-        dsc
-                .setUrl(jdbcUrl)
-                .setDriverName(jdbcDriver)
-                .setUsername(jdbcUsername)
-                .setPassword(jdbcPassword)
-//                .setTypeConvert(new MySqlTypeConvert() {
-
-        ;
-
-
-        // 包配置
-        PackageConfig pc = new PackageConfig();
-        // 父级包名，按需修改
-        pc
-                .setParent("com.cj")
-                .setController("saio.controller")
-                .setEntity("saio.entity")
-                .setService("saio.service")
-                .setServiceImpl("saio.service.impl")
-                .setMapper("saio.mapper")
-                .setXml("saio.mapper.xml")
-//                .setController("sxcx.controller")
-//                .setEntity("core.entity")
-//                .setService("sxcx.service")
-//                .setServiceImpl("sxcx.service.impl")
-//                .setMapper("sxcx.mapper")
-//                .setXml("sxcx.mapper.xml")
-                // 设置模块名, 会在parent包下生成一个指定的模块包
-                .setModuleName(null);
-
-
-        // 策略配置
-        StrategyConfig strategy = new StrategyConfig();
-        strategy.setInclude(tables)
-                .setNaming(NamingStrategy.underline_to_camel)
-                .setColumnNaming(NamingStrategy.underline_to_camel)
-                //生成 @RestController 控制器
-                .setRestControllerStyle(true)
-                // 写于父类中的公共字段
-                .setSuperEntityColumns("id")
-                // Controller驼峰连字符，如开启，则requestMapping由 helloWorld 变为 hello-world 默认false
-                .setControllerMappingHyphenStyle(false)
-                .setTablePrefix(pc.getModuleName() + "_")
-                // 开启后将使用lombok注解代替set-get方法，false则生成set-get方法
-                .setEntityLombokModel(true)
-                // 在实体类中移除is前缀
-                .setEntityBooleanColumnRemoveIsPrefix(true);
-
-        new AutoGenerator()
-                .setGlobalConfig(gc)
-                .setDataSource(dsc)
-                .setPackageInfo(pc)
-                .setStrategy(strategy)
-                .setTemplateEngine(new FreemarkerTemplateEngine())
-                .execute();
-    }
-
-}
-
+//
+////        String[] tables = {
+////                "wx_ecg_msg",
+////        };
+//
+//        // ==================================================
+//
+//
+//        // 全局配置
+//        GlobalConfig gc = new GlobalConfig();
+//        String projectPath = System.getProperty("user.dir");
+//        gc.setOutputDir(projectPath + "/src/main/java")
+//                .setAuthor(author)
+//                .setBaseResultMap(true)
+//                .setBaseColumnList(true)
+//                // 生成完毕后是否打开输出目录
+//                .setOpen(false)
+//                // 为true时生成entity将继承Model类，单类即可完成基于单表的业务逻辑操作，按需开启
+//                .setActiveRecord(false)
+//                //使用Date作为时间类型
+//                .setDateType(DateType.ONLY_DATE)
+//                .setSwagger2(true);
+//
+//
+//        // 数据源配置
+//        DataSourceConfig dsc = new DataSourceConfig();
+//        dsc
+//                .setUrl(jdbcUrl)
+//                .setDriverName(jdbcDriver)
+//                .setUsername(jdbcUsername)
+//                .setPassword(jdbcPassword)
+////                .setTypeConvert(new MySqlTypeConvert() {
+//
+//        ;
+//
+//
+//        // 包配置
+//        PackageConfig pc = new PackageConfig();
+//        // 父级包名，按需修改
+//        pc
+//                .setParent("com.cj")
+//                .setController("saio.controller")
+//                .setEntity("saio.entity")
+//                .setService("saio.service")
+//                .setServiceImpl("saio.service.impl")
+//                .setMapper("saio.mapper")
+//                .setXml("saio.mapper.xml")
+////                .setController("sxcx.controller")
+////                .setEntity("core.entity")
+////                .setService("sxcx.service")
+////                .setServiceImpl("sxcx.service.impl")
+////                .setMapper("sxcx.mapper")
+////                .setXml("sxcx.mapper.xml")
+//                // 设置模块名, 会在parent包下生成一个指定的模块包
+//                .setModuleName(null);
+//
+//
+//        // 策略配置
+//        StrategyConfig strategy = new StrategyConfig();
+//        strategy.setInclude(tables)
+//                .setNaming(NamingStrategy.underline_to_camel)
+//                .setColumnNaming(NamingStrategy.underline_to_camel)
+//                //生成 @RestController 控制器
+//                .setRestControllerStyle(true)
+//                // 写于父类中的公共字段
+//                .setSuperEntityColumns("id")
+//                // Controller驼峰连字符，如开启，则requestMapping由 helloWorld 变为 hello-world 默认false
+//                .setControllerMappingHyphenStyle(false)
+//                .setTablePrefix(pc.getModuleName() + "_")
+//                // 开启后将使用lombok注解代替set-get方法，false则生成set-get方法
+//                .setEntityLombokModel(true)
+//                // 在实体类中移除is前缀
+//                .setEntityBooleanColumnRemoveIsPrefix(true);
+//
+//        new AutoGenerator()
+//                .setGlobalConfig(gc)
+//                .setDataSource(dsc)
+//                .setPackageInfo(pc)
+//                .setStrategy(strategy)
+//                .setTemplateEngine(new FreemarkerTemplateEngine())
+//                .execute();
+//    }
+//
+//}
+//
---
+++ src/main/java/com/cj/mpgen/util/gen/CodeGenerator35.java
@@ -0,0 +1,80 @@
+package com.cj.mpgen.util.gen;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.TemplateType;
+import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
+import com.baomidou.mybatisplus.generator.config.querys.MySqlQuery;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import com.baomidou.mybatisplus.generator.keywords.MySqlKeyWordsHandler;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+
+
+/**
+ * @description: 代码生成器
+ **/
+@Slf4j
+public class CodeGenerator35 {
+
+    public static void main(String[] args) {
+
+        String projectPath = System.getProperty("user.dir").replaceAll("\\\\","/") + "/src/main";
+        DataSourceConfig build = new DataSourceConfig.Builder("jdbc:mysql://47.103.64.182:63306/ujiev2", "root", "rootpass")
+                .typeConvert(new MySqlTypeConvert())
+                .keyWordsHandler(new MySqlKeyWordsHandler())
+                .build();
+
+
+        FastAutoGenerator.create("jdbc:mysql://47.103.64.182:63306/ujiev2", "root", "rootpass")
+                .globalConfig(builder -> {
+                    builder.author("acheck") // 设置作者
+                            .fileOverride() // 覆盖已生成文件
+                            .enableSwagger() // 开启 swagger 模式
+                            .author("admin")
+                            .enableSwagger()
+                            .dateType(DateType.ONLY_DATE)
+                            .outputDir(projectPath + "/java"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("com.cj") // 设置父包名
+                            .moduleName("sbasic") // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, projectPath + "/resources/xml")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("article","v6_aio_data") // 设置需要生成的表名
+                            .addTablePrefix("t_", "c_") // 设置过滤表前缀
+
+
+                            .controllerBuilder()
+                            .enableRestStyle()
+
+                            .serviceBuilder()
+                            .formatServiceFileName("%sService")
+                            .formatServiceImplFileName("%sServiceImp")
+
+                            .mapperBuilder()
+                            .superClass(BaseMapper.class)
+                            .enableMapperAnnotation()
+                            .enableBaseResultMap()
+                            .enableBaseColumnList()
+
+                    ;
+
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+
+    }
+
+
+}
+
