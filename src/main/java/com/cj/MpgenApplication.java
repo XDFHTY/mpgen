e639dd39f30b4b69565051ebd492ce199dcbb8c7
a24568744 <2393803535@qq.com>
2023-01-13 14:28:15 +0800

    update db

--- src/main/java/com/cj/MpgenApplication.java
+++ src/main/java/com/cj/MpgenApplication.java
@@ -2,13 +2,15 @@ package com.cj;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.util.TimeZone;
 
-@SpringBootApplication
 @MapperScan({"com.cj.*.mapper"})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MpgenApplication {
 
     public static void main(String[] args) {
