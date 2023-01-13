5005b617fa8a951c984770d813aadbbd7d4d7424
a24568744 <2393803535@qq.com>
2023-01-13 15:27:46 +0800

    upd file

--- mp/src/main/java/com/cj/mpgen/util/Passutil.java
+++ mp/src/main/java/com/cj/mpgen/util/Passutil.java
@@ -1,15 +1,17 @@
 package com.cj.mpgen.util;
 
+import com.cj.mpgen.util.md5.Md5Utils;
+
 import static com.cj.mpgen.util.md5.Md5Utils.MD5Encode;
 
 public class Passutil {
 
     public static String enPass(long id, String saltval, String plPass) {
-        return MD5Encode(id + saltval + plPass, "UTF-8", true);
+        return Md5Utils.MD5Encode(id + saltval + plPass, "UTF-8", true);
     }
 
     public static String enPass(String saltval, String plPass) {
-        return MD5Encode(saltval + plPass, "UTF-8", true);
+        return Md5Utils.MD5Encode(saltval + plPass, "UTF-8", true);
     }
 
     public static boolean checkPass(Long id, String saltval, String plPass, String enPass) {
@@ -24,6 +26,6 @@ public class Passutil {
     }
 
     public static void main(String[] args) {
-        System.out.println(MD5Encode("superadmins"));
+        System.out.println(Md5Utils.MD5Encode("superadmins"));
     }
 }
--- mp/src/main/java/com/cj/mpgen/util/gen/CodeGenerator35.java
+++ mp/src/main/java/com/cj/mpgen/util/gen/CodeGenerator35.java
@@ -2,20 +2,13 @@ package com.cj.mpgen.util.gen;
 
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.OutputFile;
-import com.baomidou.mybatisplus.generator.config.TemplateType;
 import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
-import com.baomidou.mybatisplus.generator.config.querys.MySqlQuery;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import com.baomidou.mybatisplus.generator.engine.BeetlTemplateEngine;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import com.baomidou.mybatisplus.generator.keywords.MySqlKeyWordsHandler;
-import com.baomidou.mybatisplus.generator.FastAutoGenerator;
-import com.baomidou.mybatisplus.generator.config.OutputFile;
-import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
@@ -31,7 +24,7 @@ public class CodeGenerator35 {
 
     public static void main(String[] args) {
 
-        String projectPath = System.getProperty("user.dir").replaceAll("\\\\","/") + "/src/main";
+        String projectPath = System.getProperty("user.dir").replaceAll("\\\\","/") + "/mp/src/main";
         DataSourceConfig build = new DataSourceConfig.Builder("jdbc:mysql://47.103.64.182:63306/ujiev2", "root", "rootpass")
                 .typeConvert(new MySqlTypeConvert())
                 .keyWordsHandler(new MySqlKeyWordsHandler())
